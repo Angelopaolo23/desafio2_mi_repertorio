d0ba3bce5359a617daf8257d0d3c67a0f0762fc0
AngeloPaolo23 <angelopaolo0223@gmail.com>
2023-05-21 23:26:27 -0400

    FINAL CHANGES

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const PORT = 3000;
+const camposIncompletos = "Debes completar todos los campos del formulario para agregar una cancion";
 app.use(express.json());
 app.listen(PORT, console.log(`Servidor encendido en puerto ${PORT}!`));
 
@@ -14,9 +15,14 @@ app.get('/canciones', (req, res) => {
 app.post('/canciones', (req, res) => {
     const cancion = req.body;
     const repertorio = JSON.parse(fs.readFileSync('repertorio.json'));
-    repertorio.push(cancion);
-    fs.writeFileSync('repertorio.json', JSON.stringify(repertorio));
-    res.send('Cancion agregada exitosamente!')
+    if (cancion.titulo != '' & cancion.artista != '' & cancion.tono != '') {
+        repertorio.push(cancion);
+        fs.writeFileSync('repertorio.json', JSON.stringify(repertorio));
+        res.status(201).send('Cancion agregada exitosamente!')
+    }
+    else {
+        res.status(400).send(camposIncompletos)
+    }
 });
 
 app.delete('/canciones/:id', (req, res) => {
@@ -25,7 +31,7 @@ app.delete('/canciones/:id', (req, res) => {
     const index = repertorio.findIndex(p => p.id == id);
     repertorio.splice(index, 1);
     fs.writeFileSync('repertorio.json', JSON.stringify(repertorio));
-    res.send('Cancion eliminada correctamente')
+    res.status(200).send('Cancion eliminada correctamente')
 });
 
 app.put('/canciones/:id', (req, res) => {
@@ -33,9 +39,15 @@ app.put('/canciones/:id', (req, res) => {
     const cancionEditada = req.body;
     const repertorio = JSON.parse(fs.readFileSync('repertorio.json'));
     const index = repertorio.findIndex(p => p.id == id);
+    if (cancionEditada.titulo != '' & cancionEditada.artista != '' & cancionEditada.tono != ''){
     repertorio[index] = cancionEditada;
     fs.writeFileSync('repertorio.json', JSON.stringify(repertorio));
-    res.send('Cancion editada correctamente')
+    res.status(200).send('Cancion editada correctamente')
+    }
+    else {
+        res.status(400).send(camposIncompletos)
+    }
+    
 });
 
 app.get('/', (req, res) => {
---
+++ readme.md
@@ -0,0 +1,5 @@
+Esta aplicacion corresponde al segundo Desafio del modulo Backend con Node y Express de la carrera Full Stack Developer en Javascript de Desafio Latam.
+
+La aplicacion consiste en un formulario para ingresar canciones a tu gusto a un repertorio que las listara, se deben ingresar 3 parametros para poder ingresar una cancion correctamente, estos son Cancion (nombre), Artista, y Tono, se desplegaran la totalidad de las canciones en la "Tabla de canciones" y podras eliminarlas o editarlas a tu gusto (OJO: debes ingresar los 3 parametros para poder Ingresar una cancion o Editarla).
+
+Los aprendizajes o conocimientos a aplicar en este desafio son el de usar Servidores Locales gracias a Express Js, la creacion de rutas con metodos HTTP (como GET, POST, DELETE y PUT) que leen y escriben sobre un archivo JSON utilizando el modulo File System, el despliegue de un Index HTML a traves de un metodo HTTP, y la manipulacion de parametros y payload gracias a las propiedades "body" y "params" de una request.
