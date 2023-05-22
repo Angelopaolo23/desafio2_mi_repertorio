59a23f795f3ec6e22ef268547e0aa60bb5fcdd31
AngeloPaolo23 <angelopaolo0223@gmail.com>
2023-05-21 22:43:11 -0400

    FUNCTIONALITY

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,37 @@ app.use(express.json());
 app.listen(PORT, console.log(`Servidor encendido en puerto ${PORT}!`));
 
 
+app.get('/canciones', (req, res) => {
+    const repertorio = JSON.parse(fs.readFileSync('repertorio.json'));
+    res.json(repertorio)
+});
 
+app.post('/canciones', (req, res) => {
+    const cancion = req.body;
+    const repertorio = JSON.parse(fs.readFileSync('repertorio.json'));
+    repertorio.push(cancion);
+    fs.writeFileSync('repertorio.json', JSON.stringify(repertorio));
+    res.send('Cancion agregada exitosamente!')
+});
+
+app.delete('/canciones/:id', (req, res) => {
+    const { id } = req.params;
+    const repertorio = JSON.parse(fs.readFileSync('repertorio.json'));
+    const index = repertorio.findIndex(p => p.id == id);
+    repertorio.splice(index, 1);
+    fs.writeFileSync('repertorio.json', JSON.stringify(repertorio));
+    res.send('Cancion eliminada correctamente')
+});
+
+app.put('/canciones/:id', (req, res) => {
+    const {id } = req.params;
+    const cancionEditada = req.body;
+    const repertorio = JSON.parse(fs.readFileSync('repertorio.json'));
+    const index = repertorio.findIndex(p => p.id == id);
+    repertorio[index] = cancionEditada;
+    fs.writeFileSync('repertorio.json', JSON.stringify(repertorio));
+    res.send('Cancion editada correctamente')
+});
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
